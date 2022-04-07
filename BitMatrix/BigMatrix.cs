f89ec55d797aa75c4e8f8da8fca06f0ccfb7beb4
maciej.firek <maciej.firek@PK4-04>
2022-04-07 16:22:06 +0200

    Dodaj pliki projektów.

---
+++ BitMatrix/BigMatrix.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitMatrix
+{
+    public class BigMatrix
+    {
+        private BitArray[] data;
+
+        public int Dimension => data.Length;
+
+        public BigMatrix(int n)
+
+        {
+
+            data = new BitArray[n];
+            for (int i = 0; i < n; i++)
+            {
+                data[i] = new BitArray(n);
+            }
+        }
+         public bool this[int i, int j]
+        {
+            get => data[i][j];
+            set { data[i][j] = value; }
+        }   
+        public override string ToString()
+        {
+            string wynik = "";
+            foreach(var wiersz in data)
+            {
+                foreach (var komorka in wiersz)
+                    wynik += komorka + " ";
+                wynik += "\n";
+            }
+            return wynik;
+        }
+    }
+
+}
---
+++ BitMatrix/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BitMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            BigMatrix m = new BigMatrix(4);
+            Console.WriteLine(m);
+            Console.WriteLine(m[2,2]);
+            m[2, 2] = true;
+            Console.WriteLine(m);
+        }
+    }
+}
